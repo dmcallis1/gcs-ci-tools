2c092ddd02270a5077ef9c1e42db7eb68bfd2bd1
Duncan McAllister <dmcallis@akamai.com>
2017-08-18 15:33:19 -0400

    Python 3 adaptations :D

--- checkPropertyVersion.py
+++ checkPropertyVersion.py
@@ -13,17 +13,17 @@ try:
     pm_config = open(sys.argv[2], 'r')
     pm_config_data = json.load(pm_config)
 except Exception as e:
-    print 'Exception encountered parsing json.'
-    print e
-    
+    print (Exception encountered parsing json.)
+    print (e)
+
 print 'Metadata version: ' + str(pm_config_data['propertyVersion'])
 print 'VCS version: ' + str(vcs_config_data['propertyVersion'])
 
 # Compare the delivery config versions
 if pm_config_data['propertyVersion'] > vcs_config_data['propertyVersion']:
     if 'CI_READY' in pm_config_data['comments']:
-        print 'Config ' + sys.argv[2] + ' is ready for integration.'
+        print (Config ' + sys.argv[2] + ' is ready for integration.)
         sys.exit(0)
 else:
-    print 'Config ' + sys.argv[2] + ' not ready for integration, exiting.'
+    print (Config ' + sys.argv[2] + ' not ready for integration, exiting.)
     sys.exit(1)
